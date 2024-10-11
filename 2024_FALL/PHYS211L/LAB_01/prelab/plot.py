d41281d82af58b68f0737be135487c3b438d1cb7
H. Ryott Glayzer <h.ryott.glayzer@ryott.gay>
2024-10-11 16:01:13 -0600

    lab1

---
+++ 2024_FALL/PHYS211L/LAB_01/prelab/plot.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+x=[1,2]
+y=[14,42]
+
+plt.plot(
+    x,
+    y,
+    color='dodgerblue'
+)
+plt.title("Displacement of a Particle")
+plt.xlabel("Time (s)")
+plt.ylabel("Displacement (m)")
+plt.show()
