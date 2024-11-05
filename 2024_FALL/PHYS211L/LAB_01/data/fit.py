68df009c48d7898bc5f2ea7a7d09ea84a584523b
H. Ryott Glayzer <h.ryott.glayzer@ryott.gay>
2024-11-05 07:40:17 -0700

    phys

--- 2024_FALL/PHYS211L/LAB_01/data/fit.py
+++ 2024_FALL/PHYS211L/LAB_01/data/fit.py
@@ -1,25 +1,56 @@
+# fit.py: fitting data for phys lab
+# copyright © 2024 H. Ryott Glayzer
+# MIT License
 import pandas as pd
+import matplotlib as mpl
+mpl.use("gtk4agg")
 from matplotlib import pyplot as plt
 import numpy as np
 import scipy.stats as stats
+from sklearn import metrics
 from sklearn.linear_model import LinearRegression
+
+def chiSquare(observed, expected):
+    """Performs a chi squared goodness-of-fit test"""
+    middleVal = []
+    for i in range(len(observed)):
+        value = ((observed[i] - expected[i])**2 / expected[i])
+        middleVal.append(value)
+
+    chiSquareStatistic = sum(middleVal)
+    return chiSquareStatistic
+
+def deviationFromModel(obs, exp):
+    partSum = []
+    for i in range(len(obs)):
+        ama = (obs[i] - exp[i])**2
+        partSum.append(ama)
+    wholeSum = sum(partSum)
+    deviation = np.sqrt(wholeSum / len(obs))
+
+    return deviation
 
 def main():
     # Import the data
     walking = pd.read_csv("walking.csv")
-    print(walking)
+    print("Walking Data Imported... ")
 
     # Make a cut of the first walk away
     walking = walking.head(146)
-    print(walking)
+    print("Making time cuts in Walking Data... ")
 
     # Convert relevant pandas crap to numpy arrays 
     walk_time = walking["Time (s) Run #1"].values.reshape(-1, 1)
     walk_pos = walking["Position (m) Run #1"].values.reshape(-1, 1)
+    walk_time.flatten()
+    #print(list(walk_time.flatten()))
     linreg = LinearRegression()
     lin_model = linreg.fit(walk_time, walk_pos)
     fit = linreg.predict(walk_time)
+    print(type(fit))
     print(f"f(x)={linreg.coef_}x+{linreg.intercept_}")
+    walk_chi2 = chiSquare(list(walk_pos.flatten()), list(fit.flatten()))
+    walk_dof = len(walk_time) - 2
 
     # Create a plot of the data
     plt.scatter(
@@ -34,18 +65,25 @@ def main():
         color='red',
         label="Regression Line"
     )
+    plt.plot(
+        [],
+        [],
+        label = f"Chi-Square/DOF = {walk_chi2} / 3"
+    )
+    print(f"Deviation from Model for Walking Model: {deviationFromModel(walk_pos.flatten(), fit.flatten())}")
     plt.xlabel("Time (sec)")
     plt.ylabel("Position (meters)")
     plt.title("Position vs Time of a Student Walking")
+    plt.legend()
     plt.show()
 
     # import the running data
     running = pd.read_csv("running.csv")
-    print(running)
+    print("Importing Running Data... ")
 
     # Make a quality cut
     running = running.head(46)
-    print(running)
+    print("Making Time Cuts in Running Data")
 
     # do a regression
     run_time = running["Time (s) Run #1"]
@@ -53,6 +91,9 @@ def main():
 
     quad_model = np.poly1d(np.polyfit(run_time, run_pos, 2))
     print(quad_model)
+
+    run_chi2 = chiSquare(run_pos, quad_model(run_time))
+    run_dof = len(run_time) - 2
 
     # Create a plot of the data
     plt.scatter(
@@ -67,6 +108,12 @@ def main():
         color='red',
         label="Quadratic Model"
     )
+    plt.plot(
+        [],
+        [],
+        label = f"Chi-Square / DOF : {run_chi2} / 3"
+    )
+    print(f"Deviation from Model for Quadratic Fit: {deviationFromModel(run_pos, quad_model(run_time))}")
     plt.title("Kinematics of a Running Student")
     plt.xlabel("Time (sec)")
     plt.ylabel("Position (meters)")
